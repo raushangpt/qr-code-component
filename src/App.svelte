9d254f1cdb294794d451caa1379a3d97638ac95a
raushangpt <rk880488@gmail.com>
2023-02-13 22:32:00 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<div class="bg-light-gray grid h-screen w-screen place-items-center">
+  <div
+    class="font-outfit max-w-xs select-none rounded-xl bg-white p-4 shadow-xl transition-all hover:shadow-2xl sm:max-w-sm sm:p-6">
+    <img class="rounded-xl" src="./images/image-qr-code.png" alt="" />
+
+    <div class="space-y-4 p-4 sm:p-6">
+      <h1 class="text-dark-blue text-center text-xl font-bold sm:text-2xl">
+        Improve your front-end skills by building projects
+      </h1>
+
+      <p class="text-grayish-blue text-center">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </div>
+  </div>
+</div>
